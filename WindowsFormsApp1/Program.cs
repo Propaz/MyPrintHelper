dd7632fe8b78947b868028cacf69037eae0921c9
Pr666 <nspropaz@gmail.com>
2018-03-04 17:41:14 +0300

    Added comment for free PVS-Studio.

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+using System;
 using System.Windows.Forms;
 
 namespace PrinterParser
@@ -17,3 +19,5 @@ namespace PrinterParser
         }
     }
 }
+
+
